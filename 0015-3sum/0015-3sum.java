32d2f5556c8f7e2c66c3f5f0b0e33abeb60c22ab
Akash Tiwari <142772439+Akasht190@users.noreply.github.com>
2024-06-06 08:58:05 +0530

    Time: 852 ms (13.88%), Space: 51.9 MB (41.53%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -3,27 +3,27 @@ class Solution {
         if(nums==null || nums.length<3){
             return new ArrayList<>();
         }
-         Set<List<Integer>> res=new HashSet<>();
-         Arrays.sort(nums);
-         int n=nums.length;
-         for(int i=0;i<n-2;i++){
-              int j=i+1;
-              int k=n-1;
-      
-              while(j<k){
-                int sum=nums[i]+nums[j]+nums[k];
-                if(sum==0){
-                    res.add(Arrays.asList(nums[i],nums[j],nums[k]));
-                    j++;
-                    k--;
-                }
-                else if(sum>0){
-                    k--;
-                }else{
-                    j++;
-                }
-              }
-         }
-         return new ArrayList<>(res);
+
+      Set<List<Integer>> res=new HashSet<>();
+      Arrays.sort(nums);
+      for(int i=0;i<nums.length-2;i++){
+        int j=i+1;
+        int k=nums.length-1;
+        while(j<k){
+            int sum=nums[i]+nums[j]+nums[k];
+            if(sum==0){
+              res.add(Arrays.asList(nums[i],nums[j],nums[k]));
+              j++;
+              k--;
+            }
+            else if(sum>0){
+                k--;
+            }else{
+                j++;
+            }
+        }
+        
+      }
+      return new ArrayList<>(res);
     }
 }
