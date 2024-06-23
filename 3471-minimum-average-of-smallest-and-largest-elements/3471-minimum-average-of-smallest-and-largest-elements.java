5509a19ef00656de6dc32a3b9773e29973c6f8d2
Akash Tiwari <142772439+Akasht190@users.noreply.github.com>
2024-06-23 23:50:26 +0530

    Time: 3 ms (100%), Space: 43.5 MB (100%) - LeetHub

--- 3471-minimum-average-of-smallest-and-largest-elements/3471-minimum-average-of-smallest-and-largest-elements.java
+++ 3471-minimum-average-of-smallest-and-largest-elements/3471-minimum-average-of-smallest-and-largest-elements.java
@@ -1,16 +1,27 @@
 class Solution {
     public double minimumAverage(int[] nums) {
-        Arrays.sort(nums);
-        int s=0;
-        int e=nums.length-1;
-        double ans=(nums[s]+nums[e])/2.0;
+        // Arrays.sort(nums);
+        // int s=0;
+        // int e=nums.length-1;
+        // double ans=(nums[s]+nums[e])/2.0;
 
-        while(s<=e){
-           // System.out.println(ans);
-            ans=Math.min(ans,((nums[s]+nums[e])/2.0));
-            s++;
-            e--;
+        // while(s<=e){
+        //    // System.out.println(ans);
+        //     ans=Math.min(ans,((nums[s]+nums[e])/2.0));
+        //     s++;
+        //     e--;
+        // }
+        // return ans;
+
+        int n=nums.length;
+        double[] avgArr=new double[n/2];
+        Arrays.sort(nums);
+
+        for(int i=0;i<n/2;i++){
+            double avg=(nums[i]+nums[n-1-i])/2.0;
+            avgArr[i]=avg;
         }
-        return ans;
+        Arrays.sort(avgArr);
+        return avgArr[0];
     }
 }
