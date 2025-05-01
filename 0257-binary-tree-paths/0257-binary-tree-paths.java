f1085d7cc641e9374a8ddb8b5bebfe55c47f68fc
Akash Tiwari <142772439+Akasht190@users.noreply.github.com>
2025-05-01 11:49:10 +0530

    Time: 2 ms (80.93%), Space: 42.2 MB (96.29%) - LeetHub

--- 0257-binary-tree-paths/0257-binary-tree-paths.java
+++ 0257-binary-tree-paths/0257-binary-tree-paths.java
@@ -13,15 +13,25 @@
  *     }
  * }
  */
+
 class Solution {
-  public List<String> binaryTreePaths(TreeNode root) {
-    List<String> answer = new ArrayList<String>();
-    if (root != null) searchBT(root, "", answer);
-    return answer;
-}
-private void searchBT(TreeNode root, String path, List<String> answer) {
-    if (root.left == null && root.right == null) answer.add(path + root.val);
-    if (root.left != null) searchBT(root.left, path + root.val + "->", answer);
-    if (root.right != null) searchBT(root.right, path + root.val + "->", answer);
-   }
+    public List<String> binaryTreePaths(TreeNode root) {
+        List<String> ans = new ArrayList<>();
+        List<String> path = new ArrayList<>();
+        dfs(root, path, ans);
+        return ans;
+    }
+
+    private void dfs(TreeNode node, List<String> path, List<String> ans) {
+        if (node == null) return;
+
+        path.add(Integer.toString(node.val));
+        if (node.left == null && node.right == null) {
+            ans.add(String.join("->", path));
+        } else {
+            dfs(node.left, path, ans);
+            dfs(node.right, path, ans);
+        }
+        path.remove(path.size() - 1);
+    }
 }
